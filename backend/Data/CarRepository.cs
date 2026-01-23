bba204f5cad4b64981fafb3b341f1fa398f2740d
Jonathan <jonomaxw3ll54@gmail.com>
2026-01-23 13:44:44 +0100

    first commit

---
+++ backend/Data/CarRepository.cs
@@ -0,0 +1,27 @@
+﻿using NexusDev.Models;
+
+namespace NexusDev.Data
+{
+    public class CarRepository
+    {
+        private static readonly List<Car> _cars = new();
+        private static int _nextId = 1;
+
+        public List<Car> GetAll() => _cars;
+        public Car? GetById(int id) => _cars.FirstOrDefault(c => c.Id == id);
+        public Car Add(Car car)
+        {
+            car.Id = _nextId++;
+            _cars.Add(car);
+            return car;
+        }
+
+        public void Update(Car car)
+        {
+            var index = _cars.FindIndex(c => c.Id == car.Id); ;
+            if (index != -1) _cars[index] = car;
+        }
+        
+        public void Delete(int id) => _cars.RemoveAll(c => c.Id == id);
+    }
+}
---
+++ backend/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace NexusDev.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        public string displayInfo() => $"{Year} {Make} {Model} - ${Price}";
+
+        /*public string displayInfo()
+        {
+            return $"{Year} {Make} {Model} - ${Price}";
+        }*/
+    }
+}
